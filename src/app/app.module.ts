0c279f935aca5ee83aff2ab4f09168e89ff8e957
Ronan Girault <girault.ronan@gmail.com>
2019-10-26 14:02:31 +0200

    init project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponentComponent } from './post-list-component/post-list-component.component';
+import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponentComponent,
+    PostListItemComponentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
