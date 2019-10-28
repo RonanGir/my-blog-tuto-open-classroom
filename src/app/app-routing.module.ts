889e4481e0a8fa195e8ad2b2e955e949a11f9d76
Ronan Girault <girault.ronan@gmail.com>
2019-10-28 20:43:13 +0100

    add navigation and routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PostListComponent } from '../post-list/post-list.component';
-import { NewPostComponent } from '../new-post/new-post.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { NewPostComponent } from './new-post/new-post.component';
 
 
 const routes: Routes = [
   { path: 'post', component: PostListComponent },
-  { path: 'post/new', component: NewPostComponent }
+  { path: 'post/new', component: NewPostComponent },
+  { path: '', component: PostListComponent },
+  { path: '**', component: PostListComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routing
 import { AppRoutingModule } from './app-routing.module';
+
+// Component
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NewPostComponent } from './new-post/new-post.component';
+
+// Services
+import { PostService } from './services/post.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +28,9 @@ import { NewPostComponent } from './new-post/new-post.component';
     AppRoutingModule,
     FontAwesomeModule
   ],
-  providers: [],
+  providers: [
+    PostService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
